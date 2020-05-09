3219c287496cbf2457f69ba4e5e1e51434756001
Eduardo Faria <z.eduardofaria@gmail.com>
2020-05-09 12:39:38 -0300

    Using useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+interface User {
+  name: string;
+  login: string;
+  avatar_url: string;
+}
+
+const App: React.FC = () => {
+  const [user, setUser] = useState<User>();
+
+  async function loadData() {
+    const response = await fetch('https://api.github.com/users/zEduardofaria');
+    const data = await response.json();
+
+    setUser(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user?.name}
     </div>
   );
 }
